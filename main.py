9336b273b48b154f6e85b3972fe934d0597b41d9
SungjiCho <whtjdwl34@naver.com>
2019-11-07 18:02:32 +0900

    feat: sunny commit

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
+from random import randint
+
+
+
 chance = 3
 answer = randint(1,100+1)
 print(answer)
-  
+
+#def guess():
+#    guess = int(input("Hi, guess the num(1 ~ 100)> "))
+#    print(["Correct" if answer == guess else "Wrong"][0])
+#for i in range(chance):
+#    guess()
+
+
+def jam_recursion(n):
+    num = int(input("Hi, guess the num(1 ~ 100)> "))
+    if n == 1:
+        print("game over!")
+        return False
+    elif num == answer:
+        print("==Correct!!==")
+        return True
+    else:
+        n -= 1
+        print("==Wrong!==")
+        jam_recursion(n)
+
+
 def guess():
     guess = int(input("Hi, guess the num(1 ~ 100)> "))
     print(["Correct" if answer == guess else "Wrong"][0])
