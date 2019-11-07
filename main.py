c3f84773a63e11018a4f8b8e24e615b86487ca80
SungjiCho <whtjdwl34@naver.com>
2019-11-07 17:26:31 +0900

    feat: while

--- main.py
+++ main.py
@@ -3,7 +3,18 @@ answer = randint(1,100+1)
 print(answer)
   
 def guess():
-   guess = int(input("Hi, guess the num(1 ~ 100)> "))
-   print(["Correct" if answer == guess else "Wrong"][0])
+    guess = int(input("Hi, guess the num(1 ~ 100)> "))
+    print(["Correct" if answer == guess else "Wrong"][0])
 for i in range(chance):
     guess()
+
+def guess_sunny(n):
+    chance = 0
+    while chance < 4:
+        guess = int(input('Guess the num(1~100)> '))
+        chance = chance + 1
+        if guess == answer:
+            print('Correct!')
+            break
+        else:
+            print('Wrong!')
